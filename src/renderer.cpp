3b6b286e0d75e4c02a80e69aee25455d0ff5ee5f
Artem Kozlov <zeqfreed@gmail.com>
2016-10-06 02:25:27 +0500

    Triangle rasterization with color interpolation

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -48,7 +48,7 @@ static inline void set_pixel(DrawingBuffer *buffer, int32_t x, int32_t y, uint32
   *((uint32_t *) &((uint8_t *)buffer->pixels)[idx]) = color;
 }
 
-static int32_t abs(int32_t v)
+static inline int32_t abs(int32_t v)
 {
   return v > 0.0 ? v : -v;
 }
@@ -96,6 +96,63 @@ static void draw_line(DrawingBuffer *buffer, int32_t x0, int32_t y0, int32_t x1,
     if (error > dx) {
       y += inc;
       error -= 2*dx;
+    }
+  }
+}
+
+typedef struct Vertex {
+  float x;
+  float y;
+  float z;
+  float r;
+  float g;
+  float b;
+  float nx;
+  float ny;
+  float nz;
+} Vertex;
+
+inline static float edge_func(float x0, float y0, float x1, float y1)
+{
+  return (x0 * y1) - (x1 * y0);
+}
+
+static void draw_triangle(DrawingBuffer *buffer, Vertex *v0, Vertex *v1, Vertex *v2)
+{
+  int minx = v0->x;
+  if (v1->x < minx) { minx = v1->x; }
+  if (v2->x < minx) { minx = v2->x; }
+
+  int miny = v0->y;
+  if (v1->y < miny) { miny = v1->y; }
+  if (v2->y < miny) { miny = v2->y; }  
+
+  int maxx = v0->x;
+  if (v1->x > maxx) { maxx = v1->x; }
+  if (v2->x > maxx) { maxx = v2->x; }  
+
+  int maxy = v0->y;
+  if (v1->y > maxy) { maxy = v1->y; }
+  if (v2->y > maxy) { maxy = v2->y; }
+
+  for (int j = miny; j <= maxy; j++) {
+    for (int i = minx; i <= maxx; i++) {
+      float tx = i + 0.5;
+      float ty = j + 0.5;
+
+      float area = edge_func(v1->x - v0->x, v1->y - v0->y, v2->x - v1->x, v2->y - v1->y);
+      float t0 = edge_func(v2->x - v1->x, v2->y - v1->y, tx - v1->x, ty - v1->y);
+      float t1 = edge_func(v0->x - v2->x, v0->y - v2->y, tx - v2->x, ty - v2->y);
+      float t2 = edge_func(v1->x - v0->x, v1->y - v0->y, tx - v0->x, ty - v0->y);
+      if (t0 >= 0.0 && t1 >= 0.0 && t2 >= 0.0) {
+        t0 /= area;
+        t1 /= area;
+        t2 /= area;
+        uint8_t r = 255.0 * (v0->r * t0 + v1->r * t1 + v2->r * t2);
+        uint8_t g = 255.0 * (v0->g * t0 + v1->g * t1 + v2->g * t2);
+        uint8_t b = 255.0 * (v0->b * t0 + v1->b * t1 + v2->b * t2);
+        set_pixel(buffer, i, j, 0xFF000000 | (r << 16) | (g << 8) | b);
+      }
     }
   }
 }
@@ -237,6 +294,16 @@ static void initialize(GlobalState *state)
 }
 
 #define WHITE 0xFFFFFFFF
+#define BLACK 0xFF000000
+
+static void clear_buffer(DrawingBuffer *buffer, uint32_t color)
+{
+  for (int j = 0; j <= buffer->height; j++) {
+    for (int i = 0; i <= buffer->width; i++) {
+      set_pixel(buffer, i, j, color);
+    }
+  }  
+}
 
 C_LINKAGE void draw_frame(GlobalState *state, DrawingBuffer *drawing_buffer)
 {
@@ -244,19 +311,29 @@ C_LINKAGE void draw_frame(GlobalState *state, DrawingBuffer *drawing_buffer)
     initialize(state);
   }
 
-  for (int j = 0; j <= drawing_buffer->height; j++) {
-    for (int i = 0; i <= drawing_buffer->width; i++) {
-      set_pixel(drawing_buffer, i, j, 0xFF000000);
-    }
-  }
+  clear_buffer(drawing_buffer, BLACK);
+
+  // Vertex v0 = {50, 50, 0, 1, 0, 0};
+  // Vertex v1 = {750, 50, 0, 0, 1, 0};
+  // Vertex v2 = {400, 550, 0, 0, 0, 1};
+  // draw_triangle(drawing_buffer, &v0, &v1, &v2);
 
   for (int fi = 0; fi < model.fcount; fi++) {
     int vi0 = model.faces[fi][0] - 1;
     int vi1 = model.faces[fi][3] - 1;
     int vi2 = model.faces[fi][6] - 1;
 
-    draw_line(drawing_buffer, model.vertices[vi0][0], model.vertices[vi0][1], model.vertices[vi1][0], model.vertices[vi1][1], WHITE);
-    draw_line(drawing_buffer, model.vertices[vi1][0], model.vertices[vi1][1], model.vertices[vi2][0], model.vertices[vi2][1], WHITE);
-    draw_line(drawing_buffer, model.vertices[vi2][0], model.vertices[vi2][1], model.vertices[vi0][0], model.vertices[vi0][1], WHITE);
+    float *sv0 = model.vertices[vi0];
+    float *sv1 = model.vertices[vi1];
+    float *sv2 = model.vertices[vi2];
+
+    Vertex v0 = {sv0[0], sv0[1], sv0[2], 1, 0, 0};
+    Vertex v1 = {sv1[0], sv1[1], sv1[2], 0, 1, 0};
+    Vertex v2 = {sv2[0], sv2[1], sv2[2], 0, 0, 1};
+    draw_triangle(drawing_buffer, &v0, &v1, &v2);
+
+    // draw_line(drawing_buffer, model.vertices[vi0][0], model.vertices[vi0][1], model.vertices[vi1][0], model.vertices[vi1][1], WHITE);
+    // draw_line(drawing_buffer, model.vertices[vi1][0], model.vertices[vi1][1], model.vertices[vi2][0], model.vertices[vi2][1], WHITE);
+    // draw_line(drawing_buffer, model.vertices[vi2][0], model.vertices[vi2][1], model.vertices[vi0][0], model.vertices[vi0][1], WHITE);
   }
 }
