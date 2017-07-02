dfcff22f09400a73c913ced9ebb706836d47d0f0
Artem Kozlov <zeqfreed@gmail.com>
2017-07-02 13:49:21 +0500

    Improve build

--- build.bat
+++ build.bat
@@ -8,5 +8,24 @@ set EXE="%EXEDIR%\evolve"
 if not exist %BUILDDIR% mkdir %BUILDDIR%
 if not exist %EXEDIR% mkdir %EXEDIR%
 
-cl.exe src\windows\main.cpp user32.lib /W4 /wd4100 /WX /Fo%BUILDDIR%\evolve.obj /Fe%EXE%
-cl.exe -Isrc src\viewer\main.cpp /std:c++14 /EHsc /LD /W4 /wd4018 /wd4100 /wd4201 /wd4505 /wd4996 /WX /Fo%BUILDDIR%\viewer.obj /Fe%BUILDDIR%\viewer.dll
+set BFLAGS=/Fd%BUILDDIR%\
+set CFLAGS=/Zi /EHsc
+set WFLAGS=/W4 /wd4018 /wd4100 /wd4201 /wd4505 /wd4996 /WX
+
+cl.exe -Isrc src\windows\main.cpp user32.lib %BFLAGS% %CFLAGS% %WFLAGS% /Fo%BUILDDIR%\evolve.obj /Fe%EXE%
+if not %errorlevel% == 0 goto :error
+
+cl.exe -Isrc src\viewer\main.cpp /LD %BFLAGS% %CFLAGS% %WFLAGS% /Fo%BUILDDIR%\viewer.obj /Fe%EXEDIR%\viewer.dll
+if not %errorlevel% == 0 goto :error
+
+REM cl.exe -Isrc src\cubes\main.cpp /LD %BFLAGS% %CFLAGS% %WFLAGS% /Fo%BUILDDIR%\cubes.obj /Fe%EXEDIR%\cubes.dll
+if not %errorlevel% == 0 goto :error
+
+goto :success
+
+:error
+REM Error
+exit 1
+
+:success
+REM Success
